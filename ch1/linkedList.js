6c8d32cbe4504bcccf2f50c598344fea99a2db71
piotrekbass <piotrek.koscielniak@gmail.com>
2014-04-02 00:46:18 +0200

    a few operations on linked list

--- ch1/linkedList.js
+++ ch1/linkedList.js
@@ -2,22 +2,36 @@
 
 function LinkedList() {
   var length = 0,
-    root = null;
+    first = null,
+    last = null;
 
   function addAtBeginning(value) {
-    var oldRoot = root;
-    root = createNode(value);
-    root.next = oldRoot;
+    var oldFirst = first;
+    first = createNode(value);
+    first.next = oldFirst;
+    if (oldFirst) {
+      oldFirst.prev = first;
+    }
     length += 1;
+    if (!oldFirst) {
+      last = first;
+    }
   }
 
   function removeAtBeginning() {
-    if (root) {
+    if (first) {
       length -= 1;
-      var poppedRoot = root;
-      root = root.next;
-      return poppedRoot.item;
+      var lastFirst = first;
+      first = lastFirst.next;
+      if (first) {
+        first.prev = null;
+      }
+      if (!first) {
+        last = null;
+      }
+      return lastFirst.item;
     }
+
     return null;
   }
 
@@ -28,7 +42,7 @@ function LinkedList() {
   }
 
   function each(func) {
-    var current = root;
+    var current = first;
     var index = 0;
     while (current) {
       func(current.item, index++);
@@ -36,13 +50,71 @@ function LinkedList() {
     }
   }
 
+  function addAtEnd(value) {
+    var oldLast = last;
+    last = createNode(value);
+    last.prev = oldLast;
+    length += 1;
+    if (oldLast) {
+      oldLast.next = last;
+    }
+    if (!first) {
+      first = last;
+    }
+  }
+
+  function getLast() {
+    if (last) {
+      return last.item;
+    }
+    return null;
+  }
+
+  function getFirst() {
+    if (first) {
+      return first.item;
+    }
+    return null;
+  }
+
+  function removeAtEnd() {
+    if (last) {
+      length -= 1;
+      var lastLast = last;
+      last = last.prev;
+      if (!last) {
+        first = null;
+      } else {
+        last.next = null;
+      }
+      return lastLast.item;
+    }
+    return null;
+  }
+
+  function reverseEach(func) {
+    var current = last;
+    var index = getCount() - 1;
+    while (current) {
+      func(current.item, index--);
+      current = current.prev;
+    }
+  }
+
+  function getCount() {
+    return length;
+  }
+
   return {
     addAtBeginning: addAtBeginning,
     removeAtBeginning: removeAtBeginning,
-    getCount: function () {
-      return length;
-    },
-    each: each
+    getCount: getCount,
+    each: each,
+    addAtEnd: addAtEnd,
+    getLast: getLast,
+    getFirst: getFirst,
+    removeAtEnd: removeAtEnd,
+    reverseEach: reverseEach
   };
 }
 module.exports = LinkedList;
--- test/ch1/linkedList.test.js
+++ test/ch1/linkedList.test.js
@@ -95,12 +95,133 @@ describe("linkedList", function () {
       });
     });
   });
+
+  describe("reverseEach", function () {
+    var values,
+      currentIndex,
+      calledNum;
+
+    beforeEach(function () {
+      values = ["test1", "test2"];
+      currentIndex = values.length - 1;
+      calledNum = 0;
+    });
+
+    it("should exists", function () {
+      expect(linkedList.reverseEach).to.be.ok;
+    });
+
+    it("callback shouldn't be called when list is empty", function () {
+      linkedList.reverseEach(function () {
+        calledNum++;
+      });
+      expect(calledNum).to.equal(0);
+    });
+
+    it("when count is 2 callback should called 2 times", function () {
+      linkedList.addAtBeginning(values[0]);
+      linkedList.addAtBeginning(values[1]);
+      expect(linkedList.getCount()).to.equal(2);
+      linkedList.reverseEach(function () {
+        calledNum++;
+      });
+      expect(calledNum).to.equal(2);
+    });
+
+  });
+
+  describe("addAtEnd", function () {
+    it("should exists", function () {
+      expect(linkedList.addAtEnd).to.be.ok;
+    });
+    it("should add element at end", function () {
+      linkedList.addAtEnd("test1");
+      linkedList.addAtEnd("test2");
+      expect(linkedList.getCount()).to.equal(2);
+      var element = linkedList.getLast();
+      expect(element).to.equal("test2");
+    });
+  });
+
+  describe("removeAtEnd", function () {
+    it("should exists", function () {
+      expect(linkedList.removeAtEnd).to.be.ok;
+    });
+    it("when list is empty should return null", function () {
+      expect(linkedList.removeAtEnd()).to.equal(null);
+    });
+    it("should remove last element", function () {
+      linkedList.addAtEnd("test1");
+      linkedList.addAtEnd("test2");
+
+      var element = linkedList.removeAtEnd();
+
+      expect(element).to.equal("test2");
+      expect(linkedList.getCount()).to.equal(1);
+    });
+  });
+
+  describe("getLast", function () {
+    it("should exists", function () {
+      expect(linkedList.getLast).to.be.ok;
+    });
+    it("should return null when list is empty", function () {
+      expect(linkedList.getLast()).to.equal(null);
+    });
+    it("should return last element", function () {
+      linkedList.addAtBeginning("test1");
+      linkedList.addAtBeginning("test2");
+      expect(linkedList.getCount()).to.equal(2);
+
+      var element = linkedList.getLast();
+
+      expect(element).to.equal("test1");
+    });
+    it("when last element was removed should return null", function () {
+      linkedList.addAtBeginning("test1");
+      linkedList.removeAtBeginning();
+      expect(linkedList.getLast()).to.equal(null);
+      expect(linkedList.getFirst()).to.equal(null);
+    });
+    it("when last element was removed getFirst and getLast should return null", function () {
+      linkedList.addAtEnd("test1");
+      linkedList.removeAtEnd();
+      expect(linkedList.getLast()).to.equal(null);
+      expect(linkedList.getFirst()).to.equal(null);
+    });
+  });
+  describe("getFirst", function () {
+    it("should exists", function () {
+      expect(linkedList.getFirst).to.be.ok;
+    });
+    it("should return null when list is empty", function () {
+      expect(linkedList.getFirst()).to.equal(null);
+    });
+    it("should return first element", function () {
+      linkedList.addAtBeginning("test1");
+      linkedList.addAtBeginning("test2");
+
+      var element = linkedList.getFirst();
+
+      expect(element).to.equal("test2");
+    });
+    it("when one element was added getLast and getFirst should return the same object", function () {
+      linkedList.addAtBeginning("test");
+      var first = linkedList.getFirst();
+      var last = linkedList.getLast();
+
+      expect(first).to.equal(last);
+    });
+    it("when one element was added getLast and getFirst should return the same object", function () {
+      linkedList.addAtEnd("test");
+      var first = linkedList.getFirst();
+      var last = linkedList.getLast();
+
+      expect(first).to.equal(last);
+    });
+  });
   describe("insertAt", function () {
   });
   describe("removeAt", function () {
-  });
-  describe("addAtEnd", function () {
-  });
-  describe("removeAtEnd", function () {
   });
 });
